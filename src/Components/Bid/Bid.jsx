1a4c73042a74bbde61deaefe1e708ad0b7cfc44e
iftakhersifat <hossensifat615@gmail.com>
2025-04-08 23:34:58 +0600

    fetch timeLeft

--- src/Components/Bid/Bid.jsx
+++ src/Components/Bid/Bid.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Bid = ({bid}) => {
-    const {image, currentBidPrice}=bid;
+    const {image, currentBidPrice, timeLeft}=bid;
     return (
         <div>
             <img className='w-[96px] h-[96px]' src={image} alt="" />
             <p>{currentBidPrice}</p>
+            <p>{timeLeft}</p>
         </div>
     );
 };
