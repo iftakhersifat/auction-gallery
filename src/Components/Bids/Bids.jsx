14d468bc41d5ed766c10865c258ac475eb7d1d6f
iftakhersifat <hossensifat615@gmail.com>
2025-04-08 23:53:14 +0600

    added key

--- src/Components/Bids/Bids.jsx
+++ src/Components/Bids/Bids.jsx
@@ -8,14 +8,16 @@ const Bids = ({bidsFetch}) => {
         <div className='px-15 bg-[#EBF0F5]'>
             <h1 className='text-[#0E2954] text-3xl mb-2'>Active Auction</h1>
             <p className='mb-8'>Discover and bid on extraordinary items</p>
+
         <div className='flex'>
             <div className="left w-[70%]">
                 <div className=''>
                 {
-                    bidsUse.map(bid=><Bid bid={bid}></Bid>)
+                    bidsUse.map(bid=><Bid key={bid.id} bid={bid}></Bid>)
                 }
                 </div>
             </div>
+
             <div className="right w-[30%] text-center">
                 <p>Fav</p>
             </div>
