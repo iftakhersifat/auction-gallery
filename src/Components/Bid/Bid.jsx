715362ce4dc2b02f73a48b534179b26cf350e535
iftakhersifat <hossensifat615@gmail.com>
2025-04-08 23:33:47 +0600

    fetch currentBidPrice

--- src/Components/Bid/Bid.jsx
+++ src/Components/Bid/Bid.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Bid = ({bid}) => {
-    const {image}=bid;
+    const {image, currentBidPrice}=bid;
     return (
         <div>
-            <img src={image} alt="" />
+            <img className='w-[96px] h-[96px]' src={image} alt="" />
+            <p>{currentBidPrice}</p>
         </div>
     );
 };
--- src/Components/Bids/Bids.jsx
+++ src/Components/Bids/Bids.jsx
@@ -5,9 +5,9 @@ const Bids = ({bidsFetch}) => {
     const bidsUse =use(bidsFetch);
     console.log(bidsUse)
     return (
-        <div className='px-15 mt-[80px] bg-[#EBF0F5]'>
-            <h1 className='text-[#0E2954] text-3xl mb-2'>Active Auction</h1>
-            <p>Discover and bid on extraordinary items</p>
+        <div className='px-15 bg-[#EBF0F5]'>
+            <h1 className='text-[#0E2954] mt-12 text-3xl mb-2'>Active Auction</h1>
+            <p className='mb-8'>Discover and bid on extraordinary items</p>
             <div>
                 {
                     bidsUse.map(bid=><Bid bid={bid}></Bid>)
