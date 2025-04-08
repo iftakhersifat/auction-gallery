39fe3d2d8f4409e2d1bd222ce3e7162f952d60da
iftakhersifat <hossensifat615@gmail.com>
2025-04-08 23:51:40 +0600

    added all fetch

--- src/Components/Bid/Bid.jsx
+++ src/Components/Bid/Bid.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Bid = ({bid}) => {
     const {image, currentBidPrice, timeLeft}=bid;
     return (
-        <div>
+        <div className='flex justify-between items-center space-y-4'>
             <img className='w-[96px] h-[96px]' src={image} alt="" />
             <p>{currentBidPrice}</p>
             <p>{timeLeft}</p>
--- src/Components/Bids/Bids.jsx
+++ src/Components/Bids/Bids.jsx
@@ -6,13 +6,21 @@ const Bids = ({bidsFetch}) => {
     console.log(bidsUse)
     return (
         <div className='px-15 bg-[#EBF0F5]'>
-            <h1 className='text-[#0E2954] mt-12 text-3xl mb-2'>Active Auction</h1>
+            <h1 className='text-[#0E2954] text-3xl mb-2'>Active Auction</h1>
             <p className='mb-8'>Discover and bid on extraordinary items</p>
-            <div>
+        <div className='flex'>
+            <div className="left w-[70%]">
+                <div className=''>
                 {
                     bidsUse.map(bid=><Bid bid={bid}></Bid>)
                 }
+                </div>
             </div>
+            <div className="right w-[30%] text-center">
+                <p>Fav</p>
+            </div>
+        </div>    
+
         </div>
     );
 };
