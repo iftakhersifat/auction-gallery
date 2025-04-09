6325c5cc6c6a1e61421610faf04c9d3386480636
iftakhersifat <hossensifat615@gmail.com>
2025-04-09 15:52:06 +0600

    added updated section

--- src/Components/Bid/Bid.jsx
+++ src/Components/Bid/Bid.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Bid = ({bid}) => {
-    const {image, currentBidPrice, timeLeft, bidsCount}=bid;
+    const {image, currentBidPrice, timeLeft, bidsCount, title}=bid;
     return (
-        <div className='flex justify-between items-center space-y-4'>
+        <div className='flex justify-between items-center space-y-4 text-[#0E2954] py-4 border-b border-[#E3EAF3] last:border-b-0'>
             
-            <img className='w-[96px] h-[96px]' src={image} alt="" />
-            <p>{currentBidPrice}</p>
-            <p>{timeLeft}</p>
-            <p>{bidsCount}</p>
+            <div className="flex space-x-4">
+         <img className="w-[64px] h-[64px]" src={image} />
+        <p className="text-sm w-[240px] ml-4">{title}</p>
+       </div>
+            <p className='text-center'>{currentBidPrice}</p>
+            <p className='text-center'>{timeLeft}</p>
+            <p className='text-center mr-6'>{bidsCount}</p>
         </div>
+      
     );
 };
 
--- src/Components/Bids/Bids.jsx
+++ src/Components/Bids/Bids.jsx
@@ -5,29 +5,39 @@ const Bids = ({bidsFetch}) => {
     const bidsUse =use(bidsFetch);
     console.log(bidsUse)
     return (
-        <div className='px-15 bg-[#EBF0F5]'>
+        <div className='px-15 py-15 bg-[#EBF0F5]'>
             <h1 className='text-[#0E2954] text-3xl mb-2'>Active Auction</h1>
             <p className='mb-8'>Discover and bid on extraordinary items</p>
 
-        <div className='flex'>
+        <div className='flex space-x-6'>
             <div className="left w-[70%] bg-[#FFFFFF] rounded-xl p-6 mb-[116px]">
 
-            <div className='flex justify-between items-center space-y-4'>
+            <div className='flex justify-between items-center space-y-4 border-b border-[#E3EAF3]'>
                 <h1>Items</h1>
-                <h1>Current Bid</h1>
+                <h1 className="ml-[300px]">Current Bid</h1>
                 <h1>Time Left</h1>
                 <h1>Bid Now</h1>
             </div>
 
-                <div className=''>
+                <div className='gap-2'>
                 {
                     bidsUse.map(bid=><Bid key={bid.id} bid={bid}></Bid>)
                 }
                 </div>
             </div>
 
-            <div className="right w-[30%] text-center">
-                <p>Fav</p>
+            {/* favorite items section */}
+            <div className="right w-[30%] bg-[#FFFFFF] py-4 rounded-xl h-[320px] text-center ">
+
+            <h1 className="text-[#0E2954] text-lg font-semibold p-2 border-b border-[#E3EAF3]">Favorite Items</h1>
+            <div className='py-2 space-y-3'>
+            <p className="text-lg font-semibold mt-10">No favorites yet</p>
+            <p className="text-lg font-semibold text-gray-500 mb-10">Click the heart icon on any item <br /> to add it to your favorites</p>
+            </div>
+          <div className="border-t border-[#E3EAF3] pt-4 font-semibold flex justify-around">
+            <p className="text-lg font-semibold">Total bids Amount</p>
+            <p className="text-2xl mt-1">$0000</p>
+          </div>
             </div>
         </div>    
 
